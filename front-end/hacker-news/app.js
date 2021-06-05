24595af3899db6360831bb2aa2a88df9e99b5a45
jinah <jinah0818@gmail.com>
2021-06-05 11:50:52 +0900

    DOM API 대치, API요청 함수 생성

--- front-end/hacker-news/app.js
+++ front-end/hacker-news/app.js
@@ -4,21 +4,23 @@ const content = document.createElement("div");
 const NEWS_ULR = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_ULR, false); //동기적 GET요청
-ajax.send();
+// API 요청 함수
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response); // JSON 형태로 응답값을 변환
-console.log(newsFeed); // 응답값을 콘솔창에서 확인
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_ULR); // JSON 형태로 응답값을 변환
 
 const ul = document.createElement("ul");
 
 // 웹 페이지에서 해시값(#) 변경될 떄 호출되는 함수
 window.addEventListener("hashchange", function () {
   const id = this.location.hash.substr(1); // 선택한 콘텐츠의 id부분만을 추출
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false); // 해당 콘텐츠 정보 요청
-  ajax.send();
 
-  const newsContent = JSON.parse(ajax.response); // 응답받은 콘텐츠를 JAON 파싱하여 저장
+  const newsContent = getData(CONTENT_URL.replace("@id", id)); // 해당 콘텐츠 정보 요청 & 응답받은 콘텐츠를 JAON 파싱하여 저장
   const title = this.document.createElement("h1");
 
   title.innerHTML = newsContent.title;
@@ -29,14 +31,16 @@ window.addEventListener("hashchange", function () {
 
 // 반복문을 통한 데이터 표현
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a"); // 엥커 태그로 감싸기
+  const div = document.createElement("div");
 
-  a.href = `#${newsFeed[i].id}`; // 링크 속성 추가
-  a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+  div.innerHTML = `
+    <li>
+        <a href="#${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+    </li>
+  `;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild); // 첫번째 자식요소
+  //   ul.appendChild(div.children[0]);
 }
 
 container.appendChild(ul);
